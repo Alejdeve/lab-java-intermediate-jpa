670a23f6ff21eb5ff9521ff50f0daebe9257307d
Manuel garcia <manuel.garcia999@hotmail.com>
2024-09-12 20:49:42 +0200

    service

---
+++ src/main/java/com/tareas/service/TaskService.java
@@ -0,0 +1,23 @@
+package com.tareas.service;
+
+import com.tareas.model.Task;
+import com.tareas.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+}
